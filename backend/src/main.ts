0bd563a2dc7d26771e0737720f0ecc25a2292086
muthumaliperera <udwasanamuthumaliperera@gmail.com>
2025-07-30 22:35:28 +0530

    CORS error fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow your frontend origin
+    credentials: true, // If you need cookies/auth
+  });
+  await app.listen(3001);
 }
 bootstrap();
